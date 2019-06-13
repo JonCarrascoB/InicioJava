ca513345ecf5a0966a404bd00990e484afdda792
Jon Carrasco <jcarrascobar@gmail.com>
2019-06-13 19:40:26 +0200

    commit4

---
+++ InicioJava/src/com/formacion/ejercicio/bucle/anidado/Ejercicio1.java
@@ -0,0 +1,27 @@
+package com.formacion.ejercicio.bucle.anidado;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+
+		 int i, suma = 0, n;
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Introduce un número: ");
+	        n = sc.nextInt();
+	        for (i = 1; i < n; i++) {  
+	            if (n % i == 0) {
+	                suma = suma + i;
+	            }
+	        }
+	        if (suma == n) {  
+	            System.out.println("Perfecto");
+	        } else {
+	            System.out.println("No es perfecto");
+
+	        }
+	        sc.close();
+	}
+
+}
